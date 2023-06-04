8da5a8748b0f27841604b9f7fdce1d9388d209a8
ajin0214 <njia214@gmail.com>
2023-06-04 16:33:25 +0900

    temp

--- app/src/main/java/com/example/myapplication/MainViewController.kt
+++ app/src/main/java/com/example/myapplication/MainViewController.kt
@@ -1,3 +1,4 @@
+
 package com.example.myapplication
 
 import android.view.View
@@ -7,6 +8,11 @@ import android.widget.TextView
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelStoreOwner
+import okhttp3.*
+import java.io.IOException
+import android.os.Handler
+import android.os.Looper
+
 
 class MainViewController(
     rootView: View,
@@ -14,13 +20,35 @@ class MainViewController(
     private val viewLifecycleObserver: LifecycleOwner
 ) {
 
-    val tvClickButton: TextView by lazy { rootView.findViewById(R.id.tv_click_button) }
-    val mainViewModel: MainViewModel by lazy { ViewModelProvider(viewModelStoreOwner).get(MainViewModel::class.java) }
-
+    private val tvClickButton: TextView by lazy { rootView.findViewById(R.id.tv_click_button) }
+    private val tvGoodbye: TextView by lazy { rootView.findViewById(R.id.tv_goodbye) }
+    private val mainViewModel: MainViewModel by lazy { ViewModelProvider(viewModelStoreOwner).get(MainViewModel::class.java) }
+    private val client = OkHttpClient()
 
     fun init() {
         initObservers()
         initView()
+
+
+        val request = Request.Builder()
+            .url("https://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=9bd03980caf9c2e3af6dd832245794cf&targetDt=20230501")
+            .build()
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                e.printStackTrace()
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                val responseBody = response.body?.string()
+
+
+                val handler = Handler(Looper.getMainLooper())
+                handler.post {
+                    val tvGoodbye: TextView = findViewById(R.id.tv_goodbye)
+                    tvGoodbye.text = responseBody
+                }
+            }
+        })
     }
 
     private fun initView() {
@@ -30,7 +58,9 @@ class MainViewController(
     }
 
     private fun initObservers() {
-        mainViewModel.isHelloWorldVisible.observe(viewLifecycleObserver) { if (it != null) setHelloWorldVisibility(isVisible = it) }
+        mainViewModel.isHelloWorldVisible.observe(viewLifecycleObserver) { isVisible ->
+            setHelloWorldVisibility(isVisible)
+        }
     }
 
     private fun onClickTvClickButton() {
