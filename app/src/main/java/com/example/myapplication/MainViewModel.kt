4eda330f27640d01a8a6d9a638d975ea1b9f09f3
ajin0214 <njia214@gmail.com>
2023-06-25 14:18:19 +0900

    [#2]retrofit 전환 및 searchDailyBoxOffice api 응답 데이터 생성

--- app/src/main/java/com/example/myapplication/MainViewModel.kt
+++ app/src/main/java/com/example/myapplication/MainViewModel.kt
@@ -1,53 +1,35 @@
 package com.example.myapplication
 
-import android.os.Handler
-import android.os.Looper
+
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import java.io.IOException
-import okhttp3.Call
-import okhttp3.Callback
-import okhttp3.OkHttpClient
-import okhttp3.Request
-import okhttp3.Response
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+
 
 class MainViewModel : ViewModel() {
 
+    private var repository: Repository? = null
+    private val _searchDailyBoxOfficeList = MutableLiveData<List<BoxOfficeDetailResult>>()
+    val searchDailyBoxOfficeList: LiveData<List<BoxOfficeDetailResult>> = _searchDailyBoxOfficeList
+
+
     init {
+        repository = Repository()
         requestSearchDailyBoxOfficeList()
     }
 
-    private val _isHelloWorldVisible = MutableLiveData(true)
-    val isHelloWorldVisible: LiveData<Boolean> = _isHelloWorldVisible
-
-    private val _searchDailyBoxOfficeList = MutableLiveData<String>()
-    val searchDailyBoxOfficeList: LiveData<String> = _searchDailyBoxOfficeList
-
-
-    fun onClickTvClickButton() {
-        _isHelloWorldVisible.value = _isHelloWorldVisible.value != true
-    }
-
     private fun requestSearchDailyBoxOfficeList() {
-        val request = Request.Builder()
-            .url("https://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=9bd03980caf9c2e3af6dd832245794cf&targetDt=20230501")
-            .build()
-        OkHttpClient().newCall(request).enqueue(object : Callback {
-            override fun onFailure(call: Call, e: IOException) {
-                e.printStackTrace()
-            }
-
-            override fun onResponse(call: Call, response: Response) {
-                val responseBody = response.body?.string()
-
-                val handler = Handler(Looper.getMainLooper())
-                handler.post {
-                    responseBody?.let {
-                        _searchDailyBoxOfficeList.value = it
-                    }
+        viewModelScope.launch {
+            try {
+                repository?.getDailyBoxOffice()?.let {
+                    _searchDailyBoxOfficeList.value = it.getDailyBoxOfficeResults() ?: emptyList()
                 }
+
+            } catch (e: Exception) {
+                // 에러 발생 시 처리
             }
-        })
+        }
     }
 }
